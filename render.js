13d0160730dafc430396769152a97998584b4460
Badis Meddouri <meddouri.badis@gmail.com>
2020-07-30 00:40:44 +0200

    Feature : Struct and theme name

--- render.js
+++ render.js
@@ -1,5 +1,6 @@
 const { promisify } = require('util');
 const fs = require('fs');
+const path = require('path'); 
 const convert = require('heic-convert');
 
 const fileUpload = document.getElementById('filePath');
@@ -7,9 +8,26 @@ const fileName = document.getElementById('fileName');
 const themeNameBtn = document.getElementById('themeNameBtn');
 const processBtn = document.getElementById('btnProcess');
 const dissapearWarning = document.getElementById('dissapearWarning');
+const dissapearSuccess = document.getElementById('dissapearSuccess');
 
 let filePath = null;
 let themeName = null;
+let defaultConfig = {
+    "imageFilename": "Catalina_*.jpg",
+    "imageCredits": "Apple",
+    "sunriseImageList": [
+        1
+    ],
+    "dayImageList": [
+        2
+    ],
+    "sunsetImageList": [
+        3
+    ],
+    "nightImageList": [
+        4
+    ]
+  };
 
 // File Upload
 document.addEventListener('drop', (e) => {
@@ -43,27 +61,48 @@ fileUpload.onchange = () => {
     }
 }
 
+// Inputs
+themeNameBtn.onkeyup = () => {
+    themeName = themeNameBtn.value;
+}
+
 // Buttons
 processBtn.onclick = () => {
     console.log("Process Start" + filePath);
-    processFile(filePath);
+    processFile(filePath).then(() => {
+        document.getElementById('successMessage').classList.remove(["dissapear"]);
+      });
 }
 
 dissapearWarning.onclick = () => {
     document.getElementById('warningMessage').classList.add(["dissapear"]);
 }
 
+dissapearSuccess.onclick = () => {
+    document.getElementById('successMessage').classList.add(["dissapear"]);
+}
+
 // Process the file
 async function processFile(filePath) {
     const inputBuffer = await promisify(fs.readFile)(filePath);
     const images = await convert.all({
-      buffer: inputBuffer, // the HEIC file buffer
-      format: 'JPEG'       // output format
+      buffer: inputBuffer,
+      format: 'JPEG'
     });
+
+    defaultConfig.imageFilename = themeName+"_*.jpg"
+
+    fs.mkdir(path.join(__dirname+"/output", themeName), (err) => { 
+        if (err) { 
+            return console.error(err); 
+        } 
+        console.log('Directory created successfully!'); 
+    }); 
    
     for (let idx in images) {
       const image = images[idx];
       const outputBuffer = await image.convert();
-      await promisify(fs.writeFile)(`./output/result-${idx}.jpg`, outputBuffer);
+      await promisify(fs.writeFile)(`./output/${themeName}/${themeName}_${parseInt(idx)+1}.jpg`, outputBuffer);
     }
+    
 };
