b05b02d48aca74b9023f31c6057e4a0a616cabc5
Badis Meddouri <meddouri.badis@gmail.com>
2020-07-29 21:10:26 +0200

    Fix : file type must be heic

--- render.js
+++ render.js
@@ -3,6 +3,7 @@ const { desktopCapturer } = require("electron")
 const fileUpload = document.getElementById('filePath');
 const fileName = document.getElementById('fileName');
 const themeNameBtn = document.getElementById('themeNameBtn');
+const processBtn = document.getElementById('btnProcess');
 let filePath = null;
 let themeName = null;
 
@@ -13,6 +14,7 @@ document.addEventListener('drop', (e) => {
 
     filePath = e.dataTransfer.files[0].path;
     fileName.innerText = e.dataTransfer.files[0].name;
+    processBtn.disabled = false;
     for (const f of e.dataTransfer.files) {
         console.log('File(s) you dragged here: ', f.path)
     }
@@ -27,6 +29,7 @@ fileUpload.onchange = () => {
     if(fileUpload.files[0] !== undefined){
         filePath = fileUpload.files[0].path;
         fileName.innerText = fileUpload.files[0].name;
+        processBtn.disabled = false;
         console.log(fileUpload.files[0].path);
     }
 }
