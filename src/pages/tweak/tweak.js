2ec08e394b94cf7d1df3c9f3145d17f0fbf0f58d
Badis MEDDOURI <meddouri.badis@gmail.com>
2020-12-06 14:20:19 +0100

    Tweak your theme : image type

--- src/pages/tweak/tweak.js
+++ src/pages/tweak/tweak.js
@@ -8,12 +8,21 @@ const fileList = {}
 // HTML elements
 const imageGallery = document.getElementById('imageGallery');
 
+function handleChangeSelect(e) {
+    console.log(fileList);
+    const { name, value } = e.target;
+    fileList[name].value = value;
+}
+
 async function readImages(dirPath) {
     fs.readdir(dirPath, function(err, items) {
         for (let i=0; i<items.length; i++) {
             if(items[i].includes(".jpg")){
                 let numImage = items[i].match(regex)[0];
-                fileList[numImage] = items[i];
+                fileList[numImage] = {
+                    name: items[i]
+                }
+
             }
         }
         console.log(fileList);
@@ -28,9 +37,20 @@ async function fillGallery(){
         let card = document.createElement('div'); card.classList.add("card");
         let cardImage = document.createElement('div'); cardImage.classList.add("card-image");
         let figureImage = document.createElement('figure'); figureImage.classList.add("image", "is-3by2");
-        let imgNew = document.createElement('img'); imgNew.src = path.join(app.getPath('userData'), "output", remote.getGlobal('sharedObject').themeName, fileList[key]);
+        let imgNew = document.createElement('img'); imgNew.src = path.join(app.getPath('userData'), "output", remote.getGlobal('sharedObject').themeName, fileList[key].name);
         let cardContent = document.createElement('div'); cardContent.classList.add("card-content", "is-overlay", "is-clipped");
         let spanTag = document.createElement('div'); spanTag.classList.add("tag", "is-info"); spanTag.innerText = key;
+
+        let selectValues = '<select name="'+key+'";><option value="">--Please choose an option--</option>\n' +
+            '    <option value="sunrise">Sunrise</option>\n' +
+            '    <option value="day">Day</option>\n' +
+            '    <option value="sunset">Sunset</option>\n' +
+            '    <option value="night">Nigh</option>\n' +
+            '    </select>';
+
+        let selectType = document.createElement('div'); selectType.classList.add("select","mt-1");
+        selectType.onchange = handleChangeSelect;
+        selectType.innerHTML = selectValues;
 
         figureImage.appendChild(imgNew);
         cardImage.appendChild(figureImage);
@@ -38,6 +58,7 @@ async function fillGallery(){
         cardImage.appendChild(cardContent);
         card.appendChild(cardImage);
         column.appendChild(card);
+        column.appendChild(selectType);
         imageGallery.appendChild(column);
     }
 }
