5c8e8a29745aa06dd519dbd13df6e2e95f992ca7
Badis MEDDOURI <meddouri.badis@gmail.com>
2020-12-06 12:33:19 +0100

    Show images of theme

--- src/pages/tweak/tweak.js
+++ src/pages/tweak/tweak.js
@@ -10,7 +10,7 @@ const imageGallery = document.getElementById('imageGallery');
 
 async function readImages(dirPath) {
     fs.readdir(dirPath, function(err, items) {
-        for (var i=0; i<items.length; i++) {
+        for (let i=0; i<items.length; i++) {
             if(items[i].includes(".jpg")){
                 let numImage = items[i].match(regex)[0];
                 fileList[numImage] = items[i];
@@ -22,16 +22,20 @@ async function readImages(dirPath) {
 }
 
 async function fillGallery(){
-    for(var key in fileList){
+    for(let key in fileList){
         console.log(key + ' - ' + fileList[key]);
         let column = document.createElement('div'); column.classList.add("column");
         let card = document.createElement('div'); card.classList.add("card");
         let cardImage = document.createElement('div'); cardImage.classList.add("card-image");
         let figureImage = document.createElement('figure'); figureImage.classList.add("image", "is-3by2");
         let imgNew = document.createElement('img'); imgNew.src = path.join(app.getPath('userData'), "output", remote.getGlobal('sharedObject').themeName, fileList[key]);
+        let cardContent = document.createElement('div'); cardContent.classList.add("card-content", "is-overlay", "is-clipped");
+        let spanTag = document.createElement('div'); spanTag.classList.add("tag", "is-info"); spanTag.innerText = key;
 
         figureImage.appendChild(imgNew);
         cardImage.appendChild(figureImage);
+        cardContent.appendChild(spanTag);
+        cardImage.appendChild(cardContent);
         card.appendChild(cardImage);
         column.appendChild(card);
         imageGallery.appendChild(column);
